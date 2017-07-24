b4e04d0c5ae8e9703fc7eca7d5c080a62e9def9c
Dan Cobb <cobbdb@gmail.com>
2017-07-24 13:44:30 -0700

    CSNPAM-48 support for postscribe. Resolves #1.

--- src/util/create-script.js
+++ src/util/create-script.js
@@ -1,11 +1,31 @@
+var postscribe = require('postscribe');
+
 /**
  * @param {string} id Player's HTML id.
  */
 module.exports = function (id) {
+    // Create the script element.
     var script = document.createElement('script');
     script.setAttribute('data-anvp', JSON.stringify({
         pInstance: id
     }));
     script.src = 'https://w3.cdn.anvato.net/player/prod/v3/scripts/anvload.js';
-    document.write(script.outerHTML);
+
+    /**
+     * Write the script to the DOM. Special scrubbing required to support postscribe.
+     * @see https://github.com/coxmediagroup/cm-anvato/issues/1
+     */
+    postscribe(document.body, script.outerHTML, {
+        beforeWriteToken: function (token) {
+            var anvp = token.attrs['data-anvp'];
+            if (anvp) {
+                anvp = anvp.replace(/(&quot\;)/g, '"');
+                token.attrs['data-anvp'] = anvp;
+            }
+            return token;
+        },
+        error: function (err) {
+            console.error('!!!!!\tVIDEO ERROR:', err);
+        }
+    });
 };
