302a996724583d6e088bc80997e64fbb2e80bd51
Dan Cobb <cobbdb@gmail.com>
2017-07-31 14:48:09 -0700

    CSNPAM-48 VIDEO: anvload.js intermittently not loading

--- src/util/create-script.js
+++ src/util/create-script.js
@@ -1,37 +1,10 @@
-var postscribe = require('postscribe'),
-    $ = require('jquery');
-
 /**
  * @param {string} id Player's HTML id.
  */
 module.exports = function (id) {
-    // Create the script element.
-    var script = document.createElement('script');
-    script.setAttribute('data-anvp', JSON.stringify({
-        pInstance: id
-    }));
-    script.src = 'https://w3.cdn.anvato.net/player/prod/v3/scripts/anvload.js';
-
-    /**
-     * Write the script to the DOM. Special scrubbing required to support postscribe.
-     * @see https://github.com/coxmediagroup/cm-anvato/issues/1
-     */
-    $(window).on('load', function () {
-        postscribe(document.body, script.outerHTML, {
-            beforeWriteToken: function (token) {
-                var anvp;
-                if (token && token.attrs) {
-                    anvp = token.attrs['data-anvp'];
-                    if (anvp) {
-                        anvp = anvp.replace(/(&quot\;)/g, '"');
-                        token.attrs['data-anvp'] = anvp;
-                    }
-                }
-                return token;
-            },
-            error: function (err) {
-                console.error('!!!!!\tVIDEO ERROR:', err);
-            }
-        });
-    });
+    // Create the script element and add to the DOM.
+    var script = '<scr' + 'ipt';
+    script += ' src="https://w3.cdn.anvato.net/player/prod/v3/scripts/anvload.js"';
+    script += ' data-anvp=\'{"pInstance":"' + id + '"}\'></scr' + 'ipt>';
+    document.write(script);
 };
