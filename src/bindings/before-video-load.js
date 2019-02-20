26d72c4d82b209d4989923bfd19734545d63dbf7
Dave Young <dyoung@groupcse.com>
2019-02-20 11:07:49 -0500

    adding test for existence of dfp plugin (will be absent if "no ads rule" is applied to video)

--- src/bindings/before-video-load.js
+++ src/bindings/before-video-load.js
@@ -8,7 +8,7 @@ module.exports = function (player, id, container) {
 
     player.on('beforeVideoLoad', function (video, initConfig) {
         // Check if this video should show pre-roll.
-        if (options.noads || initConfig.plugins.dfp.clientSide.adTagUrl === 'MCP_ENFORCED_DISABLE') {
+        if (options.noads || typeof initConfig.plugins.dfp === 'undefined' || initConfig.plugins.dfp.clientSide.adTagUrl === 'MCP_ENFORCED_DISABLE') {
             // Disable ads for this video.
             console.warn('[cmAnvato] Pre-roll DISABLE was requested for player', id);
             return false;
